d0460a91639a4ea5e608acabc1187673b569dc35
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-08-01 12:54:13 -0700

    routing-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
+import { Switch, Route } from 'react-router';
 import './App.css';
+import Home from './components/Home'
+import Login from './components/Login'
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Switch>
+        <Route exact path='/home'>
+          <Home />
+        </Route>
+        <Route exact path='/'>
+          <Login />
+        </Route>
+      </Switch>
     </div>
   );
 }
