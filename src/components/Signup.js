779f90f265215cdd347a77fe24d1cde4aeff3bf5
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-08-01 15:55:22 -0700

    onchange commit

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,18 +1,26 @@
+import axios from "axios";
 export default function SignUp(){
+    let newValue = { username:"", password:""}
     const submit = (e) =>{
         e.preventDefault();
-        console.log('account created')
+       // https://ft-potluck-planner-backend.herokuapp.com/api/auth/register
     }
+    const inputChangeHandler =(e)=>{
+        if(e.target.name == "username" || e.target.name == "password"){
+            newValue[e.target.name] = e.target.value
+        }
+    }
+
     return<div>
         <form onSubmit={submit}>
             <div>
         <label>Create Username:
-                <input name="username" type="text" defaultValue=""/>
+                <input name="username" type="text" defaultValue="" onChange={inputChangeHandler}/>
             </label>
             </div>
             <div>
             <label>Create Password:
-                <input name="password" type="password" defaultValue=""/>
+                <input name="password" type="password" defaultValue="" onChange={inputChangeHandler}/>
             </label>
             </div>
             <div>
